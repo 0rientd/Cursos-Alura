5a42cfdd39e484fc44b78f208d185e6ac9d71ebe
hackerorientado <hackerorientado@protonmail.com>
2019-09-28 22:32:01 -0300

    Commit de teste

--- Python/conta.py
+++ Python/conta.py
@@ -1,5 +1,6 @@
-
-
 def criar_conta(numero, titular, saldo, limite):
     conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
     return conta
+
+if (__name__ == "__main__"):
+    criar_conta()
